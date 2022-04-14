8c3dadd614b35ecbf16dfe00e286662f7fb363f3
Justin Joyce <justin.joyce@joocer.com>
2022-04-14 16:16:16 +0100

    doc

--- opteryx/config.py
+++ opteryx/config.py
@@ -2,9 +2,11 @@ import os
 import dotenv 
 from pathlib import Path
 
-env_path = Path(".") / ".env"
-dotenv.load_dotenv(dotenv_path=env_path)
-
+try:
+    env_path = Path(".") / ".env"
+    dotenv.load_dotenv(dotenv_path=env_path)
+except:
+    pass # fail quietly
 
 INTERNAL_BATCH_SIZE: int = int(os.environ.get("INTERNAL_BATCH_SIZE", 500))
 
