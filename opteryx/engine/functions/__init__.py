93a20c03f34d54fe7db7a8f5b32c2e56345e6ddc
Justin Joyce <justin.joyce@joocer.com>
2022-08-18 21:28:32 +0100

    FEATURE/#42 - 318

--- opteryx/engine/functions/__init__.py
+++ opteryx/engine/functions/__init__.py
@@ -225,7 +225,7 @@ FUNCTIONS = {
     "PI": (None, _repeat_no_parameters(number_functions.pi)),
     # DATES & TIMES
     "DATE_TRUNC": (None, _iterate_double_parameter_field_second(date_trunc),),
-    "TIME_BUCKET": (None, compute.floor_temporal),
+    "TIME_BUCKET": (None, date_functions.date_floor),
     "DATEDIFF": (pyarrow.float64(), date_functions.date_diff,),
     "DATEPART": (None, date_functions.date_part,),
     "DATE_FORMAT": (None, date_functions.date_format),
