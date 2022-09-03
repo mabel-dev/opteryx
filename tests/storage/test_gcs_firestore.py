2daf329b73832c9c9409b1a44c72e4c854ba8a89
Justin Joyce <justin.joyce@joocer.com>
2022-09-03 16:52:14 +0100

    tests

---
+++ tests/storage/test_gcs_firestore.py
@@ -0,0 +1,40 @@
+"""
+Test we can read from GCS
+"""
+import os
+import sys
+
+sys.path.insert(1, os.path.join(sys.path[0], "../.."))
+
+import opteryx
+
+from opteryx.connectors import FireStoreStorage
+
+
+
+def test_firestore_storage():
+
+    opteryx.register_store("dwarves", FireStoreStorage)
+
+    conn = opteryx.connect()
+
+    # SELECT EVERYTHING
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM dwarves;")
+    rows = list(cur.fetchall())
+    assert len(rows) == 7
+
+    # PROCESS THE DATA IN SOME WAY
+    cur = conn.cursor()
+    cur.execute(
+        f"SELECT COUNT(*) FROM dwarves GROUP BY actor;"
+    )
+    rows = list(cur.fetchall())
+    assert len(rows) == 2
+
+    conn.close()
+
+
+if __name__ == "__main__":  # pragma: no cover
+    test_firestore_storage()
+    print("✅ okay")
