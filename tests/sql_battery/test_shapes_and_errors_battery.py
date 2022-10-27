9e35d8dde572547186955d05e18da7372558a7bd
Justin Joyce <justin.joyce@joocer.com>
2022-10-27 11:33:14 +0100

    FEATURE/#547

--- tests/sql_battery/test_shapes_and_errors_battery.py
+++ tests/sql_battery/test_shapes_and_errors_battery.py
@@ -731,8 +731,6 @@ STATEMENTS = [
         ("SET @v = 1; SELECT * FROM (SELECT @v);", 1, 1, None),
         # [#561] HASH JOIN with an empty table
         ("SELECT * FROM $planets LEFT JOIN (SELECT planetId as id FROM $satellites WHERE id < 0) USING (id)", 0, 1, None),
-        # Large results can't be added to pages [#453]
-        ("SELECT SHA512(column_0) FROM FAKE(150000, 1)", 150000, 1, None),
         # Low cardinality INNER JOINS blow memory [#444]
         ("SELECT COUNT(*) FROM (SELECT * FROM testdata.formats.parquet WITH(NO_PARTITION) LIMIT 50) INNER JOIN testdata.formats.parquet WITH(NO_PARTITION) USING (user_verified)", 1, 1, None),
     ]
