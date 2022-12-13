58d93bd4a37842b1eb7fa5ee438ee9086fd33c20
Justin Joyce <justin.joyce@joocer.com>
2022-12-13 23:32:11 +0000

    #708

--- opteryx/connectors/capabilities/predicate_pushable.py
+++ opteryx/connectors/capabilities/predicate_pushable.py
@@ -1,4 +1,18 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import abc
+
+from typing import List
 
 from opteryx.exceptions import NotSupportedError
 from opteryx.managers.expression import NodeType
@@ -62,14 +76,12 @@ class PredicatePushable(abc.ABC):
             return None
         return dnf
 
-    _predicates = []
+    _predicates: List = []
 
     def push_predicate(self, predicate):
         """
         Push the predicate to the set - this creates a set of ANDs
         """
-        if not self.can_push_selection:
-            return False
         dnfed = PredicatePushable.to_dnf(predicate)
         if dnfed is not None:
             # we can't push all predicates everywhere
