c06342b3f442d81ca07e4c0f18735114184ea5e2
Justin Joyce <justin.joyce@joocer.com>
2023-03-06 14:00:59 +0000

    #906

--- opteryx/components/dataframe/__init__.py
+++
@@ -1,7 +0,0 @@
-def _table_to_tuples(table):
-    # Get the schema and columns from the table
-    schema = table.schema
-    columns = [table.column(i) for i in schema.names]
-    # Create a list of tuples from the columns
-    rows = [tuple(col[i].as_py() for col in columns) for i in range(table.num_rows)]
-    return rows
