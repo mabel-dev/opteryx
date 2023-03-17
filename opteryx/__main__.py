6942604c9e4a7c1568b2044ff9f6d280154d67b9
Justin Joyce <justin.joyce@joocer.com>
2023-03-17 00:48:16 +0000

    #942

--- opteryx/__main__.py
+++ opteryx/__main__.py
@@ -18,6 +18,7 @@ A command line utility for Opteryx
 import time
 
 import orjson
+import orso
 import typer
 
 import opteryx
@@ -25,7 +26,6 @@ from opteryx.components.sql_rewriter.sql_rewriter import clean_statement
 from opteryx.components.sql_rewriter.sql_rewriter import remove_comments
 from opteryx.components.sql_rewriter.temporal_extraction import extract_temporal_filters
 from opteryx.third_party import sqloxide
-from opteryx.utils import display
 
 
 # fmt:off
@@ -56,7 +56,7 @@ def main(
     duration = time.monotonic_ns() - start
 
     if o == "console":
-        print(display.ascii_table(table, limit=-1, display_width=table_width, colorize=color, max_column_width=max_col_width))
+        print(orso.display.ascii_table(table, limit=-1, display_width=table_width, colorize=color, max_column_width=max_col_width))
         if stats:
             print(f"{duration/1e9}")
         return
