1b9e103ce9fde2b0b7357cdf3099c907b1e1ec14
Justin Joyce <justin.joyce@joocer.com>
2023-04-26 17:24:11 +0100

    #1010

--- tests/query_planning/test_predicate_pushdown_orc.py
+++ tests/query_planning/test_predicate_pushdown_orc.py
@@ -8,8 +8,9 @@ sys.path.insert(1, os.path.join(sys.path[0], "../.."))
 
 import opteryx
 from opteryx import config
+from tests.tools import skip_on_partials
 
-
+@skip_on_partials
 def test_predicate_pushdowns_blobs_orc():
     os.environ["GCP_PROJECT_ID"] = "mabeldev"
 
