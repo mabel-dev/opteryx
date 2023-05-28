fbd6524105a2181981d3701e8ac62e77236923bf
Justin Joyce <justin.joyce@joocer.com>
2023-05-29 00:01:43 +0100

    #1017

--- opteryx/connectors/sample_data.py
+++ opteryx/connectors/sample_data.py
@@ -28,16 +28,19 @@ WELL_KNOWN_DATASETS = {
 }
 
 
+def suggest(dataset):
+    from opteryx.utils import fuzzy_search
+
+    known_datasets = (k for k in WELL_KNOWN_DATASETS if k != "$no_table")
+    suggestion = fuzzy_search(dataset, known_datasets)
+    if suggestion is not None:
+        return (
+            f"The requested dataset, '{dataset}', could not be found. Did you mean '{suggestion}'?"
+        )
+
+
 class SampleDataConnector(BaseConnector):
     __mode__ = "Internal"
-
-    def suggest(self, dataset):
-        from opteryx.utils import fuzzy_search
-
-        known_datasets = (k for k in WELL_KNOWN_DATASETS if k != "$no_table")
-        suggestion = fuzzy_search(dataset, known_datasets)
-        if suggestion is not None:
-            return f"The requested dataset, '{dataset}', could not be found. Did you mean '{suggestion}'?"
 
     def read_dataset(self, dataset_name: str) -> "DatasetReader":
         return SampleDatasetReader(dataset_name.lower())
@@ -45,7 +48,7 @@ class SampleDataConnector(BaseConnector):
     def get_dataset_schema(self, dataset_name: str) -> RelationSchema:
         data_provider = WELL_KNOWN_DATASETS.get(dataset_name.lower())
         if data_provider is None:
-            suggestion = self.suggest(dataset_name.lower())
+            suggestion = suggest(dataset_name.lower())
             raise DatasetNotFoundError(message=suggestion, dataset=dataset_name)
         return data_provider.schema
 
@@ -78,6 +81,6 @@ class SampleDatasetReader(DatasetReader):
 
         data_provider = WELL_KNOWN_DATASETS.get(self.dataset_name)
         if data_provider is None:
-            suggestion = self.suggest(self.dataset_name.lower())
+            suggestion = suggest(self.dataset_name.lower())
             raise DatasetNotFoundError(message=suggestion, dataset=self.dataset_name)
         return data_provider.read()
