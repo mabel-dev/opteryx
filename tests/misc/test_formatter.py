23251df1f69ee1713bc3688d767ff0105e8beae1
Justin Joyce <justin.joyce@joocer.com>
2023-06-08 23:10:21 +0100

    #1017

--- tests/misc/test_formatter.py
+++ tests/misc/test_formatter.py
@@ -11,8 +11,8 @@ def test_format_sql():
     formatted_sql = format_sql(sql)
     assert (
         formatted_sql
-        == "\n\x1b[38;2;139;233;253mSELECT\x1b[0m * \x1b[38;2;139;233;253mFROM\x1b[0m mytable \x1b[0m"
-    ), formatted_sql.encode()
+        == "\x1b[38;2;139;233;253mSELECT\x1b[0m * \x1b[38;2;139;233;253mFROM\x1b[0m mytable \x1b[0m"
+    ), str(formatted_sql.encode()) + "\n" + formatted_sql
 
 
 if __name__ == "__main__":
