c1bd779d013cdf3ea105da881beeb98cdbd4b046
Justin Joyce <justin.joyce@joocer.com>
2023-06-18 12:18:05 +0100

    #1083

--- opteryx/shared/query_statistics.py
+++ opteryx/shared/query_statistics.py
@@ -10,72 +10,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from dataclasses import dataclass
+
+from collections import defaultdict
 
 
-@dataclass
 class _QueryStatistics:
-    """
-    Data object to collect information during query execution.
-
-    Implemented as a singleton rather than having to pass the Statistics modeal around the plan
-    and execution, you only need the query id (qid) and the stats model can be returned.
-    """
-
     def __init__(self):
-        self._messages = []
-
-        self.count_blobs_found: int = 0
-        self.count_data_blobs_read: int = 0
-        self.count_non_data_blobs_read: int = 0
-        self.bytes_read_data: int = 0
-        self.bytes_processed_data: int = 0
-        self.count_blobs_ignored_frames: int = 0
-        self.rows_read: int = 0
-        self.columns_read: int = 0
-
-        self.read_errors: int = 0
-
-        self.count_unknown_blob_type_found: int = 0
-        self.count_control_blobs_found: int = 0
-        self.bytes_read_control: int = 0
-
-        self.partitions_found: int = 0
-        self.partitions_scanned: int = 0
-        self.partitions_read: int = 0
-        self.time_scanning_partitions: int = 0
-
-        self.segments_scanned: int = 0
-
-        self.collections_read: int = 0
-        self.document_chunks: int = 0
-
-        self.tables_read: int = 0
-
-        self.time_data_read: int = 0
-
-        self.cache_hits: int = 0
-        self.cache_misses: int = 0
-        self.cache_oversize: int = 0
-        self.cache_errors: int = 0
-        self.cache_evictions: int = 0
-
-        # time spent on various steps
-        self.time_planning: int = 0
-        self.time_selecting: int = 0
-        self.time_aggregating: int = 0
-        self.time_ordering: int = 0
-        self.time_evaluating: int = 0
-        self.time_optimizing: int = 0
-        self.time_defragmenting: int = 0
-        self.time_rewriting: int = 0
-        self.time_limiting: int = 0
-
-        self.start_time: int = 0
-        self.end_time: int = 0
-
-        self.chunk_splits: int = 0
-        self.chunk_merges: int = 0
+        self._stats = defaultdict(int)
+        self._stats["messages"] = []
 
     def _ns_to_s(self, nano_seconds):
         """convert elapsed ns to s"""
@@ -83,59 +25,39 @@ class _QueryStatistics:
             return 0
         return nano_seconds / 1e9
 
+    def __getattr__(self, attr):
+        if attr == "messages":
+            if not "messages" in self._stats:
+                return []
+        return self._stats[attr]
+
+    def __setattr__(self, attr, value):
+        if attr == "_stats":
+            super().__setattr__(attr, value)
+        else:
+            self._stats[attr] = value
+
     def add_message(self, message: str):
         """collect warnings"""
-        if message not in self._messages:
-            self._messages.append(message)
-
-    @property
-    def messages(self):
-        return self._messages
+        if not "messages" in self._stats:
+            self._stats["messages"] = [message]
+        else:
+            self._stats["messages"].append(message)
 
     def as_dict(self):
         """
         Return statistics as a dictionary
         """
-        return {
-            "count_blobs_found": self.count_blobs_found,
-            "count_data_blobs_read": self.count_data_blobs_read,
-            "count_non_data_blobs_read": self.count_non_data_blobs_read,
-            "count_blobs_ignored_frames": self.count_blobs_ignored_frames,
-            "count_unknown_blob_type_found": self.count_unknown_blob_type_found,
-            "count_control_blobs_found": self.count_control_blobs_found,
-            "read_errors": self.read_errors,
-            "bytes_read_control": self.bytes_read_control,
-            "bytes_read_data": self.bytes_read_data,
-            "bytes_processed_data": self.bytes_processed_data,
-            "rows_read": self.rows_read,
-            "columns_read": self.columns_read,
-            "time_data_read": self._ns_to_s(self.time_data_read),
-            "time_total": self._ns_to_s(self.end_time - self.start_time),
-            "time_planning": self._ns_to_s(self.time_planning),
-            "time_scanning_partitions": self._ns_to_s(self.time_scanning_partitions),
-            "time_selecting": self._ns_to_s(self.time_selecting),
-            "time_aggregating": self._ns_to_s(self.time_aggregating),
-            "time_ordering": self._ns_to_s(self.time_ordering),
-            "time_evaluating": self._ns_to_s(self.time_evaluating),
-            "time_optimizing": self._ns_to_s(self.time_optimizing),
-            "time_defragmenting": self._ns_to_s(self.time_defragmenting),
-            "time_rewriting": self._ns_to_s(self.time_rewriting),
-            "time_limiting": self._ns_to_s(self.time_limiting),
-            "partitions_found": self.partitions_found,
-            "partitions_scanned": self.partitions_scanned,
-            "partitions_read": self.partitions_read,
-            "segments_scanned": self.segments_scanned,
-            "cache_hits": self.cache_hits,
-            "cache_misses": self.cache_misses,
-            "cache_oversize": self.cache_oversize,
-            "cache_errors": self.cache_errors,
-            "cache_evictions": self.cache_evictions,
-            "collections_read": self.collections_read,
-            "tables_read": self.tables_read,
-            "document_chunks": self.document_chunks,
-            "chunk_splits": self.chunk_splits,
-            "chunk_merges": self.chunk_merges,
-        }
+        stats_dict = dict(self._stats)
+        for k, v in stats_dict.items():
+            # times are recorded in ns but reported in seconds
+            if k.startswith("time_"):
+                stats_dict[k] = self._ns_to_s(v)
+        stats_dict["time_total"] = self._ns_to_s(
+            stats_dict.get("end_time", 0) - stats_dict.get("start_time", 0)
+        )
+        stats_dict["messages"] = stats_dict.get("messages", [])
+        return stats_dict
 
 
 class QueryStatistics(_QueryStatistics):
