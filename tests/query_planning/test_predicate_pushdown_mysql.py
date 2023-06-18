b636b5def263cb0f1dd4689b14c99ccbb060866b
Justin Joyce <justin.joyce@joocer.com>
2023-06-18 18:23:47 +0100

    #1083

--- tests/query_planning/test_predicate_pushdown_mysql.py
+++ tests/query_planning/test_predicate_pushdown_mysql.py
@@ -37,25 +37,25 @@ def test_predicate_pushdowns_mysql_eq():
     cur.execute("SET enable_optimizer = false; SELECT * FROM mysql.planets WHERE name = 'Mercury';")
     # if we disable pushdown, we read all the rows from the source and we do the filter
     assert cur.rowcount == 1, cur.rowcount
-    assert cur.stats["rows_read"] == 9, cur.stats
+    assert cur.stats.get("rows_read", 0) == 9, cur.stats
 
     cur = conn.cursor()
     cur.execute("SELECT * FROM mysql.planets WITH(NO_PUSH_SELECTION) WHERE name = 'Mercury';")
     # if we disable pushdown, we read all the rows from the source and we do the filter
     assert cur.rowcount == 1, cur.rowcount
-    assert cur.stats["rows_read"] == 9, cur.stats
+    assert cur.stats.get("rows_read", 0) == 9, cur.stats
 
     cur = conn.cursor()
     cur.execute("SELECT * FROM mysql.planets WHERE name = 'Mercury';")
     # when pushdown is enabled, we only read the matching rows from the source
     assert cur.rowcount == 1, cur.rowcount
-    assert cur.stats["rows_read"] == 1, cur.stats
+    assert cur.stats.get("rows_read", 0) == 1, cur.stats
 
     cur = conn.cursor()
     cur.execute("SELECT * FROM mysql.planets WHERE name = 'Mercury' AND gravity = 3.7;")
     # test with a two part filter
     assert cur.rowcount == 1, cur.rowcount
-    assert cur.stats["rows_read"] == 1, cur.stats
+    assert cur.stats.get("rows_read", 0) == 1, cur.stats
 
     cur = conn.cursor()
     cur.execute(
@@ -63,13 +63,13 @@ def test_predicate_pushdowns_mysql_eq():
     )
     # test with A three part filter
     assert cur.rowcount == 0, cur.rowcount
-    assert cur.stats["rows_read"] == 0, cur.stats
+    assert cur.stats.get("rows_read", 0) == 0, cur.stats
 
     cur = conn.cursor()
     cur.execute("SELECT * FROM mysql.planets WHERE gravity = 3.7 AND name IN ('Mercury', 'Venus');")
     # we don't push all predicates down,
     assert cur.rowcount == 1, cur.rowcount
-    assert cur.stats["rows_read"] == 2, cur.stats
+    assert cur.stats.get("rows_read", 0) == 2, cur.stats
 
     conn.close()
 
@@ -79,35 +79,35 @@ def test_predicate_pushdowns_mysql_eq():
 def test_predicate_pushdown_mysql_other():
     res = opteryx.query("SELECT * FROM mysql.planets WHERE gravity <= 3.7")
     assert res.rowcount == 3, res.rowcount
-    assert res.stats["rows_read"] == 3, res.stats
+    assert res.stats.get("rows_read", 0) == 3, res.stats
 
     res = opteryx.query("SELECT * FROM mysql.planets WHERE name != 'Earth'")
     assert res.rowcount == 8, res.rowcount
-    assert res.stats["rows_read"] == 8, res.stats
+    assert res.stats.get("rows_read", 0) == 8, res.stats
 
     res = opteryx.query("SELECT * FROM mysql.planets WHERE name != 'E\"arth'")
     assert res.rowcount == 9, res.rowcount
-    assert res.stats["rows_read"] == 9, res.stats
+    assert res.stats.get("rows_read", 0) == 9, res.stats
 
     res = opteryx.query("SELECT * FROM mysql.planets WHERE gravity != 3.7")
     assert res.rowcount == 7, res.rowcount
-    assert res.stats["rows_read"] == 7, res.stats
+    assert res.stats.get("rows_read", 0) == 7, res.stats
 
     res = opteryx.query("SELECT * FROM mysql.planets WHERE gravity < 3.7")
     assert res.rowcount == 1, res.rowcount
-    assert res.stats["rows_read"] == 1, res.stats
+    assert res.stats.get("rows_read", 0) == 1, res.stats
 
     res = opteryx.query("SELECT * FROM mysql.planets WHERE gravity > 3.7")
     assert res.rowcount == 6, res.rowcount
-    assert res.stats["rows_read"] == 6, res.stats
+    assert res.stats.get("rows_read", 0) == 6, res.stats
 
     res = opteryx.query("SELECT * FROM mysql.planets WHERE gravity >= 3.7")
     assert res.rowcount == 8, res.rowcount
-    assert res.stats["rows_read"] == 8, res.stats
+    assert res.stats.get("rows_read", 0) == 8, res.stats
 
     res = opteryx.query("SELECT * FROM mysql.planets WHERE name LIKE '%a%'")
     assert res.rowcount == 4, res.rowcount
-    assert res.stats["rows_read"] == 4, res.stats
+    assert res.stats.get("rows_read", 0) == 4, res.stats
 
 
 if __name__ == "__main__":  # pragma: no cover
