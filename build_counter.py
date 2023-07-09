9e5dea9cb5312fd74a14ede0dcdd907ecd0e5034
joocer <justin.joyce@joocer.com>
2023-07-09 15:43:47 +0100

    0.12.0

--- build_counter.py
+++ build_counter.py
@@ -6,5 +6,5 @@ build_number = os.environ.get("GITHUB_RUN_NUMBER")
 build_numnber = int(build_number)
 
 # Save the build number to the build.py file
-with open("build.py", "w") as f:
+with open("opteryx/build.py", "w") as f:
     f.write(f"__build__ = {build_number}\n")
