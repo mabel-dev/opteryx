3f401b70b4074335d56799647ffe02e291849d1b
joocer <justin.joyce@joocer.com>
2023-11-19 01:44:59 +0000

    #1267

--- tests/storage/test_sql_postgres.py
+++ tests/storage/test_sql_postgres.py
@@ -9,32 +9,34 @@ sys.path.insert(1, os.path.join(sys.path[0], "../.."))
 import opteryx
 from opteryx.connectors import SqlConnector
 
-MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
-MYSQL_USER = os.environ.get("MYSQL_USER")
-CONNECTION = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@mysql-3c28af34-joocer-9765.a.aivencloud.com:10067/defaultdb?charset=utf8mb4"
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
 
 
-def test_mysql_storage():
-    opteryx.register_store("mysql", SqlConnector, remove_prefix=True, connection=CONNECTION)
+def test_postgres_storage():
+    opteryx.register_store(
+        "pg",
+        SqlConnector,
+        remove_prefix=True,
+        connection=f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@trumpet.db.elephantsql.com/{POSTGRES_USER}",
+    )
 
-    results = opteryx.query("SELECT * FROM mysql.planets")
+    results = opteryx.query("SELECT * FROM pg.planets")
     assert results.rowcount == 9, results.rowcount
     assert results.columncount == 20
 
     # PROCESS THE DATA IN SOME WAY
-    results = opteryx.query("SELECT COUNT(*) FROM mysql.planets;")
+    results = opteryx.query("SELECT COUNT(*) FROM pg.planets;")
     assert results.rowcount == 1, results.rowcount
     assert results.columncount == 1
 
     # PUSH A PROJECTION
-    results = opteryx.query("SELECT name FROM mysql.planets;")
+    results = opteryx.query("SELECT name FROM pg.planets;")
     assert results.rowcount == 9, results.rowcount
     assert results.columncount == 1
 
 
 if __name__ == "__main__":  # pragma: no cover
     from tests.tools import run_tests
-
-    _mysql_create_tables()
 
     run_tests()
