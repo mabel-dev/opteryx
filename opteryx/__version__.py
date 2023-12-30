68a2ec83a015e7dc709842e964f9a1127d147110
XB500 <XB500@users.noreply.github.com>
2023-12-30 14:53:12 +0000

    Update Build Number to 156

--- opteryx/__version__.py
+++ opteryx/__version__.py
@@ -1,4 +1,4 @@
-__build__ = 155
+__build__ = 156
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
