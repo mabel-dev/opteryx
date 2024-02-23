42e528bc10ae75dfcb645b42c2fb48ffd8d2a339
joocer <justin.joyce@joocer.com>
2024-02-23 17:37:54 +0000

    #1464

--- opteryx/components/cost_based_optimizer/__init__.py
+++ opteryx/components/cost_based_optimizer/__init__.py
@@ -63,7 +63,6 @@ class CostBasedOptimizerVisitor:
             ConstantFoldingStrategy(),
             BooleanSimplificationStrategy(),
             SplitConjunctivePredicatesStrategy(),
-            RewriteInWithSingleComparitorStrategy(),
             PredicatePushdownStrategy(),
             ProjectionPushdownStrategy(),
         ]
