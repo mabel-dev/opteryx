d5794d8647dedf319d302e0c1022699db72ff64f
joocer <justin.joyce@joocer.com>
2024-03-04 19:38:48 +0000

    #1490

--- tests/storage/test_sql_duckdb.py
+++ tests/storage/test_sql_duckdb.py
@@ -12,6 +12,24 @@ import opteryx
 from opteryx.connectors import SqlConnector
 from tests.tools import create_duck_db
 
+# fmt:off
+test_cases = [
+    ("SELECT * FROM duckdb.planets", 9, 20),
+    ("SELECT * FROM duckdb.satellites", 177, 8),
+    ("SELECT COUNT(*) FROM duckdb.planets;", 1, 1),
+    ("SELECT COUNT(*) FROM duckdb.satellites;", 1, 1),
+    ("SELECT name FROM duckdb.planets;", 9, 1),
+    ("SELECT name FROM duckdb.satellites", 177, 1),
+    ("SELECT * FROM duckdb.planets, duckdb.satellites", 1593, 28),
+    ("SELECT * FROM duckdb.planets INNER JOIN $satellites ON duckdb.planets.id = $satellites.planetId;", 177, 28),
+    ("SELECT * FROM duckdb.planets INNER JOIN duckdb.satellites ON duckdb.planets.id = duckdb.satellites.planetId;", 177, 28),
+    ("SELECT * FROM duckdb.planets, duckdb.satellites WHERE duckdb.planets.id = duckdb.satellites.planetId;", 177, 28),
+    ("SELECT * FROM duckdb.planets, duckdb.satellites WHERE duckdb.planets.id = 5 AND duckdb.satellites.planetId = 5;", 67, 28),
+    ("SELECT * FROM duckdb.planets, duckdb.satellites WHERE duckdb.planets.id - duckdb.satellites.planetId = 0;", 177, 28),
+    ("SELECT * FROM duckdb.planets, duckdb.satellites WHERE duckdb.planets.id - duckdb.satellites.planetId != 0;", 1416, 28),
+]
+# fmt:on
+
 
 def test_duckdb_storage():
     create_duck_db()
@@ -22,28 +40,6 @@ def test_duckdb_storage():
         remove_prefix=True,
         connection="duckdb:///planets.duckdb",
     )
-
-    results = opteryx.query("SELECT * FROM duckdb.planets")
-    assert results.rowcount == 9, results.rowcount
-    assert results.columncount == 20
-
-    # PROCESS THE DATA IN SOME WAY
-    results = opteryx.query("SELECT COUNT(*) FROM duckdb.planets;")
-    assert results.rowcount == 1, results.rowcount
-    assert results.columncount == 1
-
-    # PUSH A PROJECTION
-    results = opteryx.query("SELECT name FROM duckdb.planets;")
-    assert results.rowcount == 9, results.rowcount
-    assert results.columncount == 1
-
-    # JOIN ON A NON SQL TABLE
-    results = opteryx.query(
-        "SELECT * FROM duckdb.planets INNER JOIN $satellites ON duckdb.planets.id = $satellites.planetId;"
-    )
-    assert results.rowcount == 177, results.rowcount
-    assert results.columncount == 28, results.columncount
-
     # PUSH - CHECK STATS THE PUSHES WORKED
     results = opteryx.query("SELECT name FROM duckdb.planets WHERE name LIKE 'Earth';")
     assert results.rowcount == 1, results.rowcount
@@ -54,6 +50,28 @@ def test_duckdb_storage():
     results = opteryx.query("SELECT * FROM duckdb.planets WHERE id > gravity")
     assert results.rowcount == 2, results.rowcount
     assert results.stats.get("rows_read", 0) == 9, results.stats
+
+
+def test_duckdb_battery():
+    from opteryx.utils.formatter import format_sql
+
+    create_duck_db()
+
+    opteryx.register_store(
+        "duckdb",
+        SqlConnector,
+        remove_prefix=True,
+        connection="duckdb:///planets.duckdb",
+    )
+
+    print(f"RUNNING DUCK BATTERY OF {len(test_cases)} TESTS")
+    for script, rows, cols in test_cases:
+        print(format_sql(script))
+        results = opteryx.query(script)
+        assert results.rowcount == rows, format_sql(script) + str(results.shape)
+        assert results.columncount == cols, format_sql(script) + str(results.shape)
+
+    print()
 
 
 if __name__ == "__main__":  # pragma: no cover
