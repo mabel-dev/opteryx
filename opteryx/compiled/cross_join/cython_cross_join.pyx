7240453b1e40b002f19aab0dca2b73e98d1e3b68
joocer <justin.joyce@joocer.com>
2024-03-18 22:30:22 +0000

    #1526

--- opteryx/compiled/cross_join/cython_cross_join.pyx
+++ opteryx/compiled/cross_join/cython_cross_join.pyx
@@ -1,43 +1,44 @@
 from libc.stdlib cimport malloc, free
 import numpy as np
 cimport numpy as cnp
-import cython
+cimport cython
+from libc.stdint cimport int32_t
+
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cpdef build_rows_indices_and_column(cnp.ndarray column_data):
     cdef Py_ssize_t i, total_size = 0
     cdef Py_ssize_t length
-    cdef cnp.ndarray flat_data, indices
-    # Use Py_ssize_t for compatibility with Python/Cython sizes and indices
-    cdef Py_ssize_t *lengths = <Py_ssize_t *>malloc(len(column_data) * sizeof(Py_ssize_t))
+    cdef list flat_data
+    cdef Py_ssize_t row_count = len(column_data)
+    cdef int32_t *lengths = <int32_t *>malloc(row_count * sizeof(int32_t))
     if lengths is NULL:
         raise MemoryError("Failed to allocate memory for lengths array.")
 
-    # Iterate over the column_data to calculate total_size and lengths of sub-arrays
-    for i in range(len(column_data)):
-        length = column_data[i].shape[0]
+    for i in range(row_count):
+        length = len(column_data[i])
         lengths[i] = length
         total_size += length
 
-    # If total_size is 0, handle the empty case
     if total_size == 0:
-        free(lengths)  # Remember to free the allocated memory to avoid memory leaks
-        return (np.array([], dtype=np.int64), np.array([], dtype=object))
+        free(lengths)
+        return (np.array([], dtype=np.int32), np.array([], dtype=object))
 
-    # Initialize the flat_data and indices arrays
-    flat_data = np.empty(total_size, dtype=object)
-    indices = np.empty(total_size, dtype=np.int64)
+    flat_data = [0] * total_size
+    cdef int32_t *indices = <int32_t *>malloc(total_size * sizeof(int32_t))
+    if indices is NULL:
+        raise MemoryError("Failed to allocate memory.")
 
-    # Fill flat_data and indices
-    cdef Py_ssize_t start = 0
-    for i in range(len(column_data)):
-        if column_data[i] is not None:
-            end = start + lengths[i]
-            flat_data[start:end] = column_data[i]
-            indices[start:end] = i
-            start = end
+    cdef int32_t start, end = 0
+    cdef int32_t j
+    for i in range(row_count):
+        end = start + lengths[i]
+        for j in range(start, end):
+            indices[j] = i 
+            flat_data[j] = column_data[i][j - start]
+        start = end
+    free(lengths)
 
-    free(lengths)  # Free the lengths array after use
-
-    return (indices, flat_data)
+    cdef cnp.int32_t[:] mv = <cnp.int32_t[:total_size]>indices
+    return (np.asarray(mv), flat_data)
