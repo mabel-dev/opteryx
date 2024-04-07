d8998c495b82569e7d65290f7a5156a02421644c
joocer <justin.joyce@joocer.com>
2024-04-07 11:05:32 +0100

    #1567

--- opteryx/compiled/functions/__init__.py
+++ opteryx/compiled/functions/__init__.py
@@ -3,6 +3,3 @@ from hash_table import HashSet
 from hash_table import HashTable
 from hash_table import distinct
 from ip_address import ip_in_cidr
-from varchar_array import array_encode_utf8
-from varchar_array import pack_byte_array
-from varchar_array import unpack_byte_array
