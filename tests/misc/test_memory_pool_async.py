0da7cc96befadc2093f73aee4eb495eb3b8a3412
joocer <justin.joyce@joocer.com>
2024-04-28 23:05:42 +0100

    #1613

---
+++ tests/misc/test_memory_pool_async.py
@@ -0,0 +1,73 @@
+"""
+Stress test the memory pool in an asynchronous execution environment.
+
+We add and remove items from the pool in quick succession, random sizes
+and random items removed, to see how it responds to random concurrent 
+access. This is targetting the async wrapper, but that is a relatively
+thin layer over the memory pool.
+"""
+
+import os
+import sys
+import random
+
+sys.path.insert(1, os.path.join(sys.path[0], "../.."))
+
+os.environ["OPTERYX_DEBUG"] = "1"
+
+from opteryx.shared import MemoryPool, AsyncMemoryPool
+
+
+import asyncio
+import random
+
+
+async def stress_with_random_sized_data():
+    bmp = MemoryPool(size=100000)
+    mp = AsyncMemoryPool(bmp)
+    refs = {}
+
+    async def add_random_data():
+        for _ in range(500):
+            size = random.randint(10, 100)
+            data = bytes([random.randint(0, 255) for _ in range(size)])
+            ref = await mp.commit(data)
+            if ref is not None:
+                refs[ref] = data
+            else:
+                # Memory pool is likely full, start removing
+                await remove_random_data()
+
+            # Simulate asynchronous behavior
+            await asyncio.sleep(random.random() * 0.005)
+
+    async def remove_random_data():
+        for _ in range(random.randint(10, 25)):
+            if refs:
+                ref = random.choice(list(refs.keys()))
+                correct_data = refs.pop(ref)
+                data_removed = await mp.read(ref)
+                await mp.release(ref)
+                assert data_removed == correct_data, "Data integrity check failed"
+
+    # Start tasks for adding and randomly removing data
+    tasks = [add_random_data() for _ in range(50)]  # Simulating concurrent access
+    await asyncio.gather(*tasks)
+
+    # Final cleanup: remove remaining items
+    for ref in list(refs):
+        await mp.read(ref)
+        await mp.release(ref)
+
+    # Ensure all memory is accounted for
+    assert bmp.available_space() == bmp.size, "Memory leak or fragmentation detected."
+
+
+def test_async_memorypool():
+    asyncio.run(stress_with_random_sized_data())
+
+
+if __name__ == "__main__":  # pragma: no cover
+    from tests.tools import run_tests
+
+    run_tests()
--- tests/storage/test_blob_gcs.py
+++ tests/storage/test_blob_gcs.py
@@ -1,47 +1,179 @@
-"""
-Test we can read from GCS
-"""
-
+from collections import namedtuple
 import os
 import sys
+import pytest
 
 sys.path.insert(1, os.path.join(sys.path[0], "../.."))
 
 import opteryx
 from opteryx.connectors import GcpCloudStorageConnector
+from opteryx.utils.formatter import format_sql
 
+# Define the test case structure
+TestCase = namedtuple("TestCase", "query expected_rowcount expected_columncount stats")
+
+# Example parameterization data using named tuples for clarity
 BUCKET_NAME = "opteryx"
+test_cases = [
+    TestCase(
+        query=f"SELECT * FROM {BUCKET_NAME}.space_missions;",
+        expected_rowcount=4630,
+        expected_columncount=8,
+        stats={},
+    ),
+    TestCase(
+        query=f"SELECT * FROM {BUCKET_NAME}.space_missions LIMIT 10;",
+        expected_rowcount=10,
+        expected_columncount=8,
+        stats={},
+    ),
+    TestCase(
+        query=f"SELECT kepid, kepoi_name FROM {BUCKET_NAME}.exoplanets ORDER BY kepid DESC LIMIT 5;",
+        expected_rowcount=5,
+        expected_columncount=2,
+        stats={},
+    ),
+    TestCase(
+        query=f"SELECT COUNT(*) AS Missions, Company FROM {BUCKET_NAME}.space_missions GROUP BY Company;",
+        expected_rowcount=62,
+        expected_columncount=2,
+        stats={},
+    ),
+    TestCase(
+        query=f"SELECT Company FROM {BUCKET_NAME}.space_missions WHERE Rocket_Status = 'Active';",
+        expected_rowcount=1010,
+        expected_columncount=1,
+        stats={"columns_read": 1, "rows_read": 1010},
+    ),
+    TestCase(
+        query=f"SELECT name, kepler_name FROM {BUCKET_NAME}.exoplanets AS exoplanets INNER JOIN $planets AS planets ON rowid = id",
+        expected_rowcount=9,
+        expected_columncount=2,
+        stats={},
+    ),
+    TestCase(
+        query=f"SELECT name, kepler_name FROM {BUCKET_NAME}.exoplanets AS exoplanets INNER JOIN $planets AS planets ON rowid = id LIMIT 5",
+        expected_rowcount=5,
+        expected_columncount=2,
+        stats={"columns_read": 2},
+    ),
+    TestCase(
+        query=f"""
+        SELECT 
+            l.l_orderkey, 
+            l.l_suppkey, 
+            s.s_name, 
+            s.s_address, 
+            l.l_extendedprice, 
+            l.l_discount
+        FROM 
+            opteryx.tpch.supplier s 
+            JOIN opteryx.tpch.lineitem l 
+            ON l.l_suppkey = s.s_suppkey
+        LIMIT 10;
+        """,
+        expected_rowcount=10,
+        expected_columncount=6,
+        stats={},
+    ),
+    TestCase(
+        query=f"SELECT l_orderkey, l_partkey, l_suppkey FROM opteryx.tpch.lineitem LIMIT 500;",
+        expected_rowcount=500,
+        expected_columncount=3,
+        stats={},
+    ),
+]
 
 
-def test_gcs_storage():
+@pytest.mark.parametrize("test_case", test_cases)
+def test_gcs_storage(test_case):
     opteryx.register_store(BUCKET_NAME, GcpCloudStorageConnector)
 
     conn = opteryx.connect()
-
-    # SELECT EVERYTHING
     cur = conn.cursor()
-    cur.execute(f"SELECT * FROM {BUCKET_NAME}.space_missions;")
-    assert cur.rowcount == 4630, cur.rowcount
+    cur.execute(test_case.query)
 
-    # PROCESS THE DATA IN SOME WAY
-    cur = conn.cursor()
-    cur.execute(
-        f"SELECT COUNT(*) AS Missions, Company FROM {BUCKET_NAME}.space_missions GROUP BY Company;"
-    )
-    assert cur.rowcount == 62, cur.rowcount
+    # Assertions for rowcount and columncount
+    assert (
+        cur.rowcount == test_case.expected_rowcount
+    ), f"Expected rowcount {test_case.expected_rowcount}, got {cur.rowcount}"
+    assert (
+        cur.columncount == test_case.expected_columncount
+    ), f"Expected columncount {test_case.expected_columncount}, got {cur.columncount}"
 
-    #  PUSHDOWNS (Parquet)
-    cur = conn.cursor()
-    cur.execute(f"SELECT Company FROM {BUCKET_NAME}.space_missions WHERE Rocket_Status = 'Active';")
-    assert cur.columncount == 1, cur.columncount
-    assert cur.rowcount == 1010, cur.rowcount
-    assert cur.stats["columns_read"] == 1, cur.stats  # we didn't read out Rocket_Status
-    assert cur.stats["rows_read"] == 1010, cur.stats
+    # Assertions for statistics
+    for key, expected_value in test_case.stats.items():
+        actual_value = cur.stats.get(key, None)
+        assert (
+            actual_value == expected_value
+        ), f"Stats check failed for {key}: expected {expected_value}, got {actual_value}"
 
     conn.close()
 
 
 if __name__ == "__main__":  # pragma: no cover
-    from tests.tools import run_tests
+    """
+    Running in the IDE we do some formatting - it's not functional but helps
+    when reading the outputs.
+    """
 
-    run_tests()
+    import shutil
+    import time
+
+    from tests.tools import trunc_printable
+
+    start_suite = time.monotonic_ns()
+
+    width = shutil.get_terminal_size((80, 20))[0] - 15
+
+    passed = 0
+    failed = 0
+
+    nl = "\n"
+
+    failures = []
+
+    print(f"RUNNING BATTERY OF {len(test_cases)} Google Cloud Storage TESTS")
+    for index, test_case in enumerate(test_cases):
+
+        (statement, rows, cols, stats) = test_case
+
+        printable = statement
+        if hasattr(printable, "decode"):
+            printable = printable.decode()
+        print(
+            f"\033[38;2;255;184;108m{(index + 1):04}\033[0m"
+            f" {trunc_printable(format_sql(printable), width - 1)}",
+            end="",
+            flush=True,
+        )
+        try:
+            start = time.monotonic_ns()
+            test_gcs_storage(test_case)
+            print(
+                f"\033[38;2;26;185;67m{str(int((time.monotonic_ns() - start)/1e6)).rjust(4)}ms\033[0m ✅",
+                end="",
+            )
+            passed += 1
+            if failed > 0:
+                print(" \033[0;31m*\033[0m")
+            else:
+                print()
+        except Exception as err:
+            print(f"\033[0;31m{str(int((time.monotonic_ns() - start)/1e6)).rjust(4)}ms ❌ *\033[0m")
+            print(">", err)
+            failed += 1
+            failures.append((statement, err))
+
+    print("--- ✅ \033[0;32mdone\033[0m")
+
+    if failed > 0:
+        print("\n\033[38;2;139;233;253m\033[3mFAILURES\033[0m")
+        for statement, err in failures:
+            print(err)
+
+    print(
+        f"\n\033[38;2;139;233;253m\033[3mCOMPLETE\033[0m ({((time.monotonic_ns() - start_suite) / 1e9):.2f} seconds)\n"
+        f"  \033[38;2;26;185;67m{passed} passed ({(passed * 100) // (passed + failed)}%)\033[0m\n"
+        f"  \033[38;2;255;121;198m{failed} failed\033[0m"
+    )
