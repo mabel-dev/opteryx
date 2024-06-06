9f4c0a44b0fce9767c9c25740348179ec0423685
joocer <justin.joyce@joocer.com>
2024-06-06 20:41:04 +0100

    1700

--- opteryx/__version__.py
+++ opteryx/__version__.py
@@ -28,8 +28,8 @@ class VersionStatus(Enum):
 
 _major = 0
 _minor = 15
-_revision = 3
-_status = VersionStatus.RELEASE
+_revision = 4
+_status = VersionStatus.ALPHA
 
 __author__ = "@joocer"
 __version__ = f"{_major}.{_minor}.{_revision}" + (
