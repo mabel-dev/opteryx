68e8ab8f0161b01b91e7e245d90d44b3bc683f8d
joocer <justin.joyce@joocer.com>
2024-07-20 15:32:32 +0100

    1806

--- build-wheels.sh
+++ build-wheels.sh
@@ -11,7 +11,7 @@ cd io
 PYBIN="/opt/python/cp${PYTHON_VERSION//.}/bin"
 
 # Install necessary packages
-"${PYBIN}/pip" install -U setuptools wheel setuptools-rust numpy==1.* cython
+"${PYBIN}/python" -m pip install -U setuptools wheel setuptools-rust numpy==1.* cython
 
 # Build the wheel
 "${PYBIN}/python" setup.py bdist_wheel
