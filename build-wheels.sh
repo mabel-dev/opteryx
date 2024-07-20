7ccfcae09aa8d5fb444c3de319b43cfd53d71b7e
joocer <justin.joyce@joocer.com>
2024-07-20 10:53:59 +0100

    1806

---
+++ build-wheels.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
+export PATH="$HOME/.cargo/bin:$PATH"
+
+cd $GITHUB_WORKSPACE/io
+cd io
+
+for PYBIN in /opt/python/cp{39,310,311,312}*/bin; do
+    "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
+    "${PYBIN}/python" setup.py bdist_wheel
+done
+
+for whl in dist/*.whl; do
+    auditwheel repair "$whl" -w dist/
+done
