9e7569d129097b6c2f174f0ae0c91307ac0e5d30
joocer <justin.joyce@joocer.com>
2024-07-20 15:27:34 +0100

    1806

--- build-wheels.sh
+++ build-wheels.sh
@@ -7,9 +7,13 @@ export PATH="$HOME/.cargo/bin:$PATH"
 cd $GITHUB_WORKSPACE/io
 cd io
 
-for PYBIN in /opt/python/cp{39,310,311,312}*/bin; do
-    "${PYBIN}/pip" install -U setuptools wheel setuptools-rust numpy==1.* cython
-    "${PYBIN}/python" setup.py bdist_wheel
+PYTHON_VERSIONS=("cp39" "cp310" "cp311" "cp312")
+
+for PYBIN in /opt/python/${PYTHON_VERSIONS[@]/#/cp}/bin; do
+    if [[ "${PYBIN}" == *"${PYTHON_VERSION}"* ]]; then
+        "${PYBIN}/pip" install -U setuptools wheel setuptools-rust numpy==1.* cython
+        "${PYBIN}/python" setup.py bdist_wheel
+    fi
 done
 
 for whl in dist/*.whl; do
