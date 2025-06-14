8ed7b2bc6d3059a0e935a7f915c9ae225d6439a7
joocer <justin.joyce@joocer.com>
2025-06-14 23:32:59 +0100

    Refactor version incrementor as precommit (#2611)

--- build_counter.py
+++ build_counter.py
@@ -1,17 +1,20 @@
-import os
 
-# Read the build number from the environment variable
-build_number = os.environ.get("GITHUB_RUN_NUMBER")
+__build__ = None
 
-if build_number:
-    build_numnber = int(build_number)
+with open("opteryx/__version__.py", "r") as f:
+    contents = f.read().splitlines()[0]
+
+__build__ = contents.split("=")[-1].strip().replace("'", "").replace('"', "")
+
+if __build__:
+    __build__ = int(__build__) + 1
 
     with open("opteryx/__version__.py", "r") as f:
         contents = f.read().splitlines()[1:]
 
     # Save the build number to the build.py file
     with open("opteryx/__version__.py", "w") as f:
-        f.write(f"__build__ = {build_number}\n")
+        f.write(f"__build__ = {__build__}\n")
         f.write("\n".join(contents) + "\n")
 
 __version__ = "notset"
@@ -19,3 +22,6 @@ with open("opteryx/__version__.py", mode="r") as v:
     vers = v.read()
 exec(vers)  # nosec
 print(__version__)
+
+import subprocess
+subprocess.run(["git", "add", "opteryx/__version__.py"])
