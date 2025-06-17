52b7c5902ae4798b88d9354fe185ea21d07857bf
joocer <justin.joyce@joocer.com>
2025-06-17 09:22:28 +0100

    Manually replace values to avoid multidimenstional array confusion in numpy #2614

--- opteryx/functions/other_functions.py
+++ opteryx/functions/other_functions.py
@@ -123,6 +123,13 @@ def if_null(values, replacements):
         values = values.to_numpy(zero_copy_only=False)
 
     if len(replacements) == 1:
+        if isinstance(replacements, numpy.ndarray):
+            replacement = replacements[0]
+            return numpy.array(
+                [values[i] if is_null else replacement for i, is_null in enumerate(is_null_mask)],
+                dtype=values.dtype,
+            )
+
         replacements = numpy.full(values.shape, replacements[0], dtype=values.dtype)
 
     target_type = numpy.promote_types(values.dtype, replacements.dtype)
