f571b132a7b8435d18e4f285e98742dfc4d9ab73
joocer <justin.joyce@joocer.com>
2025-08-02 17:05:45 +0100

    remove hard coding of precision

--- opteryx/compiled/third_party/ryu.pyx
+++ opteryx/compiled/third_party/ryu.pyx
@@ -14,7 +14,7 @@ from libc.stdint cimport uint32_t
 cdef extern from "ryu.h":
     int d2fixed_buffered_n(double d, uint32_t precision, char* result)
 
-cpdef numpy.ndarray[object] format_double_array_bytes(numpy.ndarray[numpy.float64_t, ndim=1] arr):
+cpdef numpy.ndarray[object] format_double_array_bytes(numpy.ndarray[numpy.float64_t, ndim=1] arr, uint32_t precision=6):
     """
     Convert a NumPy array of float64s to a NumPy object array of bytes.
     """
@@ -26,13 +26,13 @@ cpdef numpy.ndarray[object] format_double_array_bytes(numpy.ndarray[numpy.float6
         int length
 
     for i in range(n):
-        length = d2fixed_buffered_n(arr[i], 6, buf)
+        length = d2fixed_buffered_n(arr[i], precision, buf)
         # Inline formatting instead of calling format_double()
         result_view[i] = (<bytes>buf[:length])
 
     return result
 
-cpdef numpy.ndarray[object] format_double_array_ascii(numpy.ndarray[numpy.float64_t, ndim=1] arr):
+cpdef numpy.ndarray[object] format_double_array_ascii(numpy.ndarray[numpy.float64_t, ndim=1] arr, uint32_t precision=6):
     """
     Convert a NumPy array of float64s to a NumPy object array of Python strings.
     """
@@ -44,7 +44,7 @@ cpdef numpy.ndarray[object] format_double_array_ascii(numpy.ndarray[numpy.float6
         int length
 
     for i in range(n):
-        length = d2fixed_buffered_n(arr[i], 6, buf)
+        length = d2fixed_buffered_n(arr[i], precision, buf)
         result_view[i] = (<bytes>buf[:length]).decode("ascii")
 
     return result
