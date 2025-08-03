89cf09aa411b1025e4c2c3a2e7f889017d8e38f1
joocer <justin.joyce@joocer.com>
2025-08-03 12:38:43 +0100

    Refactor timestamp parsing

--- tests/functions/test_timestamp.py
+++ tests/functions/test_timestamp.py
@@ -4,6 +4,7 @@ import sys
 sys.path.insert(1, os.path.join(sys.path[0], "../.."))
 
 import datetime
+from pytest import raises
 from opteryx.compiled.functions.timestamp import parse_iso_timestamp, parse_iso
 
 
@@ -13,17 +14,27 @@ def test_parse_timestamp():    # Test valid timestamp
     assert parse_iso_timestamp(b"2023-10-01T12:00:00Z") == 1696161600000000
 
     # Test invalid timestamp
-    try:
+    with raises(ValueError, match="Invalid ISO timestamp"):
         parse_iso_timestamp(b"invalid-timestamp")
-    except ValueError as e:
-        assert str(e) == "Invalid format – expected date-only or full timestamp", e
-
+ 
     # Test edge cases
     assert parse_iso_timestamp(b"1970-01-01 00:00:00") == 0, parse_iso_timestamp(b"1970-01-01 00:00:00")
+    assert parse_iso_timestamp(b"1970-01-01 00:00:00.000001") == 1, parse_iso_timestamp(b"1970-01-01 00:00:00.000001")
+    assert parse_iso_timestamp(b"1969-12-31 23:59:59") == -1000000, parse_iso_timestamp(b"1969-12-31 23:59:59")
+    assert parse_iso_timestamp(b"1969-12-31 23:59:59.999999") == -1, parse_iso_timestamp(b"1969-12-31 23:59:59.999999")
+    assert parse_iso_timestamp(b"1969-12-31 23:59:59.999998") == -2, parse_iso_timestamp(b"1969-12-31 23:59:59.999998")
+    assert parse_iso_timestamp(b"1969-07-20 08:17:40") == -14226140000000, parse_iso_timestamp(b"1969-07-20 08:17:40")
     assert parse_iso_timestamp(b"9999-12-31 23:59:59") == 253402300799000000, parse_iso_timestamp(b"9999-12-31 23:59:59")
 
     assert parse_iso_timestamp(b"2023-10-01") == 1696118400000000, parse_iso_timestamp(b"2023-10-01")
     assert parse_iso(b"2023-10-01") == datetime.datetime(2023,10,1,0,0,0), f"{parse_iso(b'2023-10-01')} != {datetime.datetime(2023,10,1,0,0,0)}"
+
+    # Test invalid formats
+    with raises(ValueError, match="Month must be between 1 and 12"):
+        parse_iso_timestamp(b"2023-13-01 12:00:00")
+    
+    with raises(ValueError, match="Invalid day for given month/year"):
+        parse_iso_timestamp(b"2023-02-30 12:00:00")
 
 
 if __name__ == "__main__":  # pragma: no cover
