d4d5398db5a54d072a6d95f7744415c23530b09f
joocer <justin.joyce@joocer.com>
2025-08-23 15:25:02 +0100

    Python 3.10 nuance

--- tests/sql_battery/test_battery_file_formats.py
+++ tests/sql_battery/test_battery_file_formats.py
@@ -11,7 +11,7 @@ sys.path.insert(1, os.path.join(sys.path[0], "../.."))
 import pytest
 
 import opteryx
-from tests.tools import is_pypy, is_windows
+from tests.tools import is_pypy, is_windows, is_version
 
 # fmt:off
 STATEMENTS = [
@@ -89,9 +89,9 @@ STATEMENTS = [
         ("SELECT COUNT(*) FROM 'testdata/flat/formats/psv'", 1, 1, False),
 
         # Vortex
-        ("SELECT * FROM testdata.flat.formats.vortex", 100000, 13, False),
-        ("SELECT user_name, user_verified FROM testdata.flat.formats.vortex WHERE user_name ILIKE '%news%'", 122, 2, False),
-        ("SELECT COUNT(*) FROM 'testdata/flat/formats/vortex'", 1, 1, False),
+        ("SELECT * FROM testdata.flat.formats.vortex", 100000, 13, is_version("3.10")),
+        ("SELECT user_name, user_verified FROM testdata.flat.formats.vortex WHERE user_name ILIKE '%news%'", 122, 2, is_version("3.10")),
+        ("SELECT COUNT(*) FROM 'testdata/flat/formats/vortex'", 1, 1, is_version("3.10")),
     ]
 # fmt:on
 
